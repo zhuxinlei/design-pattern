17d51974ad6fe517f759e799530240bf8325e071
朱欣磊 <zxlcjzxl@163.com>
2018-06-05 22:56:46 +0800

    没有使用观察者的方法

---
+++ observe/noObserve.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 没有观察者模式的实例
+ * User: zhuxinlei
+ * Date: 2018/6/4
+ * Time: 下午11:49
+ */
+class order{
+    public function addOrder(){
+        //发短信
+        Message::update();
+        //发邮件
+        Email::update();
+        //记日志
+        Log::update();
+    }
+}
+$order = new order();
+$order->addOrder();
+?>
